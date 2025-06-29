9b7443b2fca1966d6cc56622c845954af1e0c96e
root <root@ip-172-31-13-75.ap-south-1.compute.internal>
2025-06-29 17:26:48 +0000

    day two of scripting

---
+++ day2/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+<< comment
+
+variable testing part 2
+
+comment
+
+
+name="Pranjul Gupta"
+
+role="Cloud/Devops engineer"
+
+echo "Hello Admin $name, your postion in this company $role."
+
+echo "hello $name we have hire you for $role."
+
+
---
+++ day2/variable.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# this is testing file for shell 
+
+<< comment
+ 
+multi line comment
+
+comment
+
+
+# variable example 
+
+
+
+my_name="Pranjul Gupta"
+
+echo "hello Admin $my_name, welcome to devops journy shell scripting"
+
+echo $(date)
+
+
+
+
+
+
